4bb8684e6e4ad49539bc1d267fc28ba859a8e650
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-09-28 21:52:53 +0200

    setup initial project

---
+++ server/src/main/java/com/go4code/server/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.go4code.server;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ServerApplication.class);
+	}
+
+}
