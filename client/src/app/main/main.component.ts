3f6ec41418cd2eb426efb7d2660fbaa0a005f7ad
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-09-29 13:57:31 +0200

    small fix bugs

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
+  public previewNewCommentSection: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openNewComment() {
+    this.previewNewCommentSection === false
+      ? (this.previewNewCommentSection = true)
+      : (this.previewNewCommentSection = false);
   }
-
 }
