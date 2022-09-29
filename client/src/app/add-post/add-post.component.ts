4abb693e21b15f91fee81bef18491901ca4628ed
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-09-29 21:48:42 +0200

    added button for add post

--- client/src/app/add-post/add-post.component.ts
+++ client/src/app/add-post/add-post.component.ts
@@ -8,36 +8,35 @@ import { PostDto } from '../DTO/post.dto';
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.css']
+  styleUrls: ['./add-post.component.css'],
 })
 export class AddPostComponent implements OnInit {
-
   public newPost: PostDto;
   user: User | undefined;
 
-  constructor(private appService: AppService, private router: Router) { 
-    this.newPost = new PostDto(JSON.parse(localStorage.getItem('user') || '{}' ),'','');
+  constructor(private appService: AppService, private router: Router) {
+    this.newPost = new PostDto(
+      JSON.parse(localStorage.getItem('user') || '{}'),
+      '',
+      ''
+    );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addPost() {
-    this.user = JSON.parse(localStorage.getItem('user') || '{}' );
+    this.user = JSON.parse(localStorage.getItem('user') || '{}');
     this.newPost.user.username = this.user?.username!;
     this.newPost.user.email = this.user?.email!;
     this.newPost.user.password = this.user?.password!;
     this.appService.addPost(this.newPost).subscribe((res: Post) => {
-      let posts = res;
-      alert("Post created"); 
+      this.router.navigate(['/main']);
     });
 
-    this.newPost.user.username = "";
-    this.newPost.user.password = "";
-    this.newPost.user.email = "";
-    this.newPost.content = "";
-    this.newPost.attachment = "";
-
+    this.newPost.user.username = '';
+    this.newPost.user.password = '';
+    this.newPost.user.email = '';
+    this.newPost.content = '';
+    this.newPost.attachment = '';
   }
-
 }
