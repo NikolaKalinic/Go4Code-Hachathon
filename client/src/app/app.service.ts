637a7ce96d65ee1ac17eb50ee294db36b5157a44
Dragan Jovanovic <dragan.jovanovic.gale@gmail.com>
2022-09-29 16:45:37 +0200

    LogIn finished

--- client/src/app/app.service.ts
+++ client/src/app/app.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserDto } from './DTO/User.dto';
 import { Post } from './model/Post.model';
+import { User } from './model/User.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +12,9 @@ export class AppService {
 
   getAllPosts() {
     return this.http.get<Post[]>(`http://localhost:8080/api/posts`);
+  }
+
+  login(userDto:UserDto){
+     return this.http.post<any>(`http://localhost:8080/api/login`,userDto);
   }
 }
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+import { UserDto } from '../DTO/User.dto';
 
 @Component({
   selector: 'app-login',
@@ -6,12 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public userDto:UserDto;
 
-  constructor() { }
+
+  constructor(private appService:AppService) { 
+
+    this.userDto={
+      username:'',
+      password:''
+    }
+  }
 
 
 
   ngOnInit(): void {
+  }
+
+  login(){
+    alert(this.userDto.username)
+    this.appService.login(this.userDto).subscribe(res=>localStorage.setItem('user', JSON.stringify(res)));
   }
 
 }
