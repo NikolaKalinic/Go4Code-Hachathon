630af2c40a17d2062dfe0988688fecf7bfc7c7d6
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-09-29 18:26:31 +0200

    small fix bugs about login

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppService } from '../app.service';
 import { UserDto } from '../DTO/User.dto';
 
@@ -9,20 +10,27 @@ import { UserDto } from '../DTO/User.dto';
 })
 export class LoginComponent implements OnInit {
   public userDto: UserDto;
+  public error: boolean;
 
-  constructor(private appService: AppService) {
+  constructor(private appService: AppService, private route: Router) {
     this.userDto = {
       username: '',
       password: '',
     };
+    this.error = false;
   }
 
   ngOnInit(): void {}
 
   login() {
     this.appService.login(this.userDto).subscribe((res) => {
-      localStorage.setItem('user', JSON.stringify(res));
-      this.appService.editUser(res);
+      if (res === null) {
+        this.error = true;
+      } else {
+        localStorage.setItem('user', JSON.stringify(res));
+        this.appService.editUser(res);
+        this.route.navigate(['/main']);
+      }
     });
   }
 }
